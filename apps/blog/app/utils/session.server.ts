8a7369bab23e090da2745da97d8cd8a26a373121
Dave Hudson <dave@applification.net>
2022-02-07 15:26:29 +0000

    Use bcryptjs instead to slime docker image size

--- apps/blog/app/utils/session.server.ts
+++ apps/blog/app/utils/session.server.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { db } from "./db.server";
 import { createCookieSessionStorage, redirect } from "remix";
 import { User } from "@prisma/client";
