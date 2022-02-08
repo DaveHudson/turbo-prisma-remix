af053e42545fc2dc8b5a7fe7e68c3f16b9d5e3db
Dave Hudson <dave@applification.net>
2022-02-08 19:17:06 +0000

    meta theme-color support

--- apps/blog/app/root.tsx
+++ apps/blog/app/root.tsx
@@ -65,6 +65,16 @@ function Document({
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="theme-color"
+          content="#f9fafb"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#111827"
+          media="(prefers-color-scheme: dark)"
+        />
         {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
