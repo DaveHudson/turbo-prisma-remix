079c99a8e92ca7a93eafe089ee85080f90aa6966
Dave Hudson <dave@applification.net>
2022-02-11 13:01:35 +0000

    post listing style improvements

--- apps/blog/app/routes/posts/index.tsx
+++ apps/blog/app/routes/posts/index.tsx
@@ -2,22 +2,21 @@ import { Link, useLoaderData } from "remix";
 import type { LoaderFunction } from "remix";
 import { getPosts, PostWithUser } from "~/utils/db/post.server";
 import dayjs from "dayjs";
-
-// TODO: implement full category tags
-// category: { name: "Case Study", href: "#", color: "bg-green-100 text-green-800" },
+import { getTags } from "~/utils/db/tag.server";
+import { Tag } from "@prisma/client";
+import RenderTags from "~/components/RenderTags";
 
 export const loader: LoaderFunction = async () => {
-  const dbPosts = getPosts();
+  const posts = await getPosts();
+  const tags = await getTags();
 
-  return dbPosts;
+  const data = { posts, tags };
+  return data;
 };
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default function Posts() {
-  const posts = useLoaderData<PostWithUser[]>();
+  const { posts, tags } =
+    useLoaderData<{ posts: PostWithUser[]; tags: Tag[] }>();
 
   return (
     <div className="px-4 pt-16 pb-20 sm:px-6 lg:px-8 lg:pt-16 lg:pb-14">
@@ -34,24 +33,12 @@ export default function Posts() {
         <div className="mt-12 grid gap-16 pt-12 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
           {posts.map((post) => (
             <div key={post.title}>
-              {/* <div>
-                <Link to={`${post.category}`} className="inline-block">
-                  <span
-                  className={classNames(
-                    post.category.color,
-                    "inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium"
-                  )}
-                  >
-                    {post.category}
-                  </span>
-                </Link>
-              </div> */}
               <Link to={`${post.id}`} className="mt-4 block">
                 <p className="text-xl font-semibold text-light dark:text-dark">
                   {post.title}
                 </p>
-                <p className="mt-3 text-base  ">{post.description}</p>
               </Link>
+              <p className="mt-3 text-base  ">{post.description}</p>
               <div className="mt-6 flex items-center">
                 <div className="flex-shrink-0">
                   <Link to={`${post.user.id}`}>
@@ -77,6 +64,9 @@ export default function Posts() {
                     <span>{post.readingTime} read</span>
                   </div>
                 </div>
+              </div>
+              <div className="flex justify-start pt-2">
+                <RenderTags tags={post.tags as []} dbTags={tags} />
               </div>
             </div>
           ))}
